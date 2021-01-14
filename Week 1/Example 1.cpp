0c56f89540f11335c05babbfd3499c295b71d98f
Robert Baxter <Robert.Baxter@georgebrown.ca>
2021-01-14 10:43:32 -0500

    Created new project. Added Example 1.cpp

---
+++ Week 1/Example 1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Circle
+{
+private:
+	double radius;
+public:
+	void setRadius(double r)
+	{
+		radius = r;
+	}
+	double calcArea()
+	{
+		return 3.14 * pow(radius, 2);
+	}
+};
+
+int main()
+{
+	Circle circle1, circle2;
+
+	circle1.setRadius(1);
+	circle2.setRadius(2.5);
+
+	//Print Area 
+
+	cout << "the area of circle 1 is: " << circle1.calcArea() << endl;
+	cout << "the area of circle 2 is: " << circle2.calcArea() << endl;
+
+	return 0;
+}
